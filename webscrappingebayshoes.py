dd96284c12eaec81f6008e99b985ca7e5ad62837
johntran996 <36209621+johntran996@users.noreply.github.com>
2023-04-09 12:42:02 +1000

    added csv creation

--- webscrappingebayshoes.py
+++ webscrappingebayshoes.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import csv
 
+csv_file = open('ebayScrape.csv', 'w', newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['url', 'listingName', 'price'])
+
+#scrapes
 url = 'https://www.ebay.com/itm/374616918598?epid=18059323676&hash=item5738e88a46:g:6t0AAOSw2MBkIa0P&amdata=enc%3AAQAHAAABAOTVLGSgN%2F%2FuDCGe%2FKDANoINVZMLr%2Bl%2Fli8styJ6aJ46zEcyV42zyNXSC34D1GbaS88UIuS%2FWbo8JQ2mSzKvVPWwIQ5hmD%2BYWcq9O904LEY2XEADSo1leViyu%2BvpNv8HVKCGyo8R2hmbGHtyqPmvBV5iRuFRCNs4jWP4PjLzCzlB2V2Pr0xexqN5hHZJqhvajvDsonzhx1F1uanZPe1N2g3k%2Bv5aiTJqSDNXadYf8cXpTtSU5veaCSeokWgN0YYxmXQb7XNxDJsCoPY4fdMdxn7xsjtxXlJ4Y0yCoOFLlXO89apR%2Fq%2BY8bOS24%2Fh8qGx9Gqv6uyoi%2FH4HYloUOPxAy8%3D%7Ctkp%3ABFBMzOSRt-xh'
-
 source = requests.get(url).text
-
 soup = BeautifulSoup(source, 'lxml')
-
 ebay = soup.find('body')
 
 shoes = ebay.find('div', id = 'mainContent')
@@ -23,10 +26,13 @@ shoepriceString = str(shoesprice)
 shoespricepattern = r'US\s+\$?\d+\.?\d*'
 
 match = re.search(shoespricepattern, shoepriceString)
+print(url)
 if match:
-    print(match.group())
+    shoePrice = match.group()
+    print(shoePrice)
 
 print(shoeslisting)
 
+csv_writer.writerow([url, shoeslisting, shoePrice])
 
-
+csv_file.close()
